3e69db07a338029218c83db92ba501a688d476b3
Florencia Morisse <florenciamorisse@gmail.com>
2024-02-10 14:01:38 -0300

    puntos verdes andando

--- script2.js
+++
@@ -1,9 +0,0 @@
-// let btnEscribinos = document.getElementById("btn_escribinos");
-// let sobreNegro = document.getElementById("sobre_negro");
-// let sobreAmarillo = document.getElementById("sobre_amarillo");
-// btnEscribinos.addEventListener("mouseover",function cambioColor(){
-
-//     sobreAmarillo.classList.toggle("display");
-//     sobreNegro.classList.toggle("display");
-// })
-
